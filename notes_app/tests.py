db5455d0d7abc81198654235b27db85f3ec6707f
javerria <javeriaiqtidar@gmail.com>
2024-06-21 16:58:25 +0500

    Debugged tests.py

--- notes_app/tests.py
+++ notes_app/tests.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+from django.contrib.auth.models import User, Group, Permission
+from django.test import TestCase, Client
 from django.urls import reverse
 from .models import StickyNote
 
@@ -17,7 +18,28 @@ class StickyNoteModelTest(TestCase):
 
 class StickyNoteViewsTest(TestCase):
     def setUp(self):
-        StickyNote.objects.create(title='Test Post Title', content='This is a test post.')
+        # Create the required group
+        self.group_name = 'test_group'
+        self.group = Group.objects.create(name=self.group_name)
+
+        # Create a user and set password
+        self.user = User.objects.create_user(username='testuser', password='12345')
+
+        # Assign the user to the group
+        self.user.groups.add(self.group)
+
+        # Assign necessary permissions to the group
+        add_permission = Permission.objects.get(codename='add_stickynote')
+        change_permission = Permission.objects.get(codename='change_stickynote')
+        delete_permission = Permission.objects.get(codename='delete_stickynote')
+        self.group.permissions.add(add_permission, change_permission, delete_permission)
+
+        # Log in the user
+        self.client = Client()
+        self.client.login(username='testuser', password='12345')
+
+        # Create a test note
+        self.note = StickyNote.objects.create(title='Test Post Title', content='This is a test post.', author=self.user.username)
 
     def test_view_all_notes(self):
         response = self.client.get(reverse('view_all_notes'))
@@ -25,32 +47,28 @@ class StickyNoteViewsTest(TestCase):
         self.assertContains(response, 'Test Post Title')
 
     def test_view_note(self):
-        test_note = StickyNote.objects.get(id=1)
-        response = self.client.get(reverse('view_note', args=[str(test_note.id)]))
+        response = self.client.get(reverse('view_note', args=[str(self.note.id)]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Test Post Title')
         self.assertContains(response, 'This is a test post.')
 
     def test_edit_note(self):
-        test_note = StickyNote.objects.get(id=1)
-        response = self.client.get(reverse('view_note', args=[str(test_note.id)]))
+        response = self.client.get(reverse('edit_note', args=[str(self.note.id)]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Test Post Title')
         self.assertContains(response, 'This is a test post.')
 
         updated_note = {
-            'title' : 'Updated Title',
-            'content' : 'This is an updated note',
-            'author' : 'There is an author now'
+            'title': 'Updated Title',
+            'content': 'This is an updated note',
+            'author': 'There is an author now'
         }
 
-        response = self.client.post(reverse('edit_note', args=[str(test_note.id)]), updated_note)
-        self.assertEqual(response.status_code, 302)
+        response = self.client.post(reverse('edit_note', args=[str(self.note.id)]), updated_note)
+        self.assertEqual(response.status_code, 302)  # Redirection after successful edit
 
-        response = self.client.get(reverse('view_note', args=[str(test_note.id)]))
+        response = self.client.get(reverse('view_note', args=[str(self.note.id)]))
         self.assertEqual(response.status_code, 200)
-
-        test_note.refresh_from_db()        
         self.assertContains(response, 'Updated Title')
         self.assertContains(response, 'This is an updated note')
         self.assertContains(response, 'There is an author now')
